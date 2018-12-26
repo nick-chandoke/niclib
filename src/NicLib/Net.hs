61487f8c5cdaaf241e8a2f1401e4b197a66bdedf
Nicholas Chandoke <nicholas.chandoke@gmail.com>
2018-12-26 13:23:18 -0600

    Fixed protocol-agnostic Option application in Net

--- src/NicLib/Net.hs
+++ src/NicLib/Net.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
+{-# language ScopedTypeVariables #-}
 
 -- | A more approachable API to @uri-bytestring@ and @req@. Import this instead of @uri-bytestring@ or @req@. And nota bene: /req/ is both the name of a package, and the premier function it exports.
 --
@@ -10,20 +11,20 @@
 -- * Because this library uses both @req@ and @uri-bytestring@, but @req@ uses only http and https schemes despite @uri-bytestring@ using any @ByteString@ as a scheme, we must limit schemes to the intersection of these two libraries: http and https.
 --     * NicLib.Net re-exports @req@'s @Scheme@ and not @uri-bytestring@'s
 --
--- Both are GADT binary coproducts:
+-- Both are binary GADT coproducts:
 --
 -- * @URIRef@: @Absolute@ | @Relative@
 -- * @Url@: @Http@ | @Https@
 --
 -- Becasue every @Url@ has a scheme, all @Urls@ created from a @URIRef@ are computed from at least one @URIRef Absolute@.
 --
--- Also, the req 'Options' opaque monoid is predicated over a scheme, but the only functions that use a specific scheme are
+-- Also, the req 'Options' opaque monoid is parameterized by a scheme, but the only functions that use a specific scheme are
 --
 -- * basicAuth
 -- * oAuth2Bearer
 -- * oAuth2Token
 --
--- and all of these are where @scheme ~ 'Https@. This means that @Option 'Http@ ⊂ @Option 'Https@. Thus all http @Option@s are assumed as http or https options.
+-- and all of these are where @scheme ~ 'Https@. It turns-out in practice that you'll only need to use @Option@s that work for both schemes; 'basicAuth' is handled by parsing those details in the @URIRef@ (see 'parseURI'), and if you're using oAuth you'll want to construct @Url@s by hand via the @(/~)@ and @(/:)@, in which case you'd import @Network.HTTP.Req@ separately from @NicLib.Net@. That's why these three functions aren't re-exported by this module.
 module NicLib.Net
 ( -- * Parse
   parseURI
@@ -40,8 +41,8 @@ module NicLib.Net
 , MonadHttp(..)
 , HttpConfig(..)
 -- ** Request
-, req
-, reqBr
+-- , req
+, reqBc
 , req'
 , Req
 , runReq
@@ -56,7 +57,7 @@ module NicLib.Net
 , ReqBodySource(..)
 , ReqBodyMultipart
 , reqBodyMultipart
--- ** Options & Query
+-- ** Options
 , Option
 , responseTimeout
 , httpVersion
@@ -64,11 +65,6 @@ module NicLib.Net
 , cookieJar
 , decompress
 , basicProxyAuth
-, oAuth1
--- *** HTTPS-Exclusive Options
-, basicAuth
-, oAuth2Bearer
-, oAuth2Token
 -- *** Response Accessors
 , responseBody
 , responseStatusCode
@@ -82,7 +78,6 @@ module NicLib.Net
 , jsonResponse
 , BsResponse
 , bsResponse
-, responseBodySource
 -- ** HTTP Methods
 , GET(..)
 , POST(..)
@@ -128,14 +123,13 @@ module NicLib.Net
 ) where
 
 -- base
--- import Data.Trie (Trie)
--- import qualified Data.Trie as Trie
+import Control.Applicative (empty)
+import Control.Arrow
 import Data.Char (toLower)
-import Data.Proxy
-import Data.Either (either)
 import Data.Foldable (foldl')
 import Data.Maybe (fromJust)
-import Control.Applicative (empty)
+import Data.Proxy
+import Data.Bifunctor (bimap)
 
 -- text
 import Data.Text (Text)
@@ -151,7 +145,11 @@ import qualified Data.ByteString.Char8 as BSC'
 
 -- misc
 import Control.Exception.Safe -- safe-exceptions
-import URI.ByteString hiding (parseURI, Scheme(..)) -- uri-bytestring
+import Control.Monad.Catch as Catch -- exceptions. Apparently this is necessary, as instancing MonadThrow cannot be done with importing just safe-exceptions
+import Conduit hiding (throwM) -- conduit
+
+-- uri-bytestring
+import URI.ByteString hiding (parseURI, Scheme(..))
 import qualified URI.ByteString as U
 
 -- req and req-conduit
@@ -165,6 +163,9 @@ data NetException
     deriving (Show, Typeable)
 
 instance Exception NetException
+
+instance MonadThrow Req where
+    throwM = liftIO . Catch.throwM
 
 {-
 instance MonadHttp _ where
@@ -208,22 +209,6 @@ instance MonadHttp _ where
         _   -> "th"
 -}
 
-{- Function for testing this library!
--- | Given the number of things I'll be creating ExceptT's for, maybe I should have [forall a. Show a => a] as the ExceptT parameter.
-getExample :: _
-getExample = do
-    exampleBase <- parseURI "https://google..com/"
-    url <- parseRelTo exampleBase "http://wiki.c2.com/?WhyWeLoveLisp"
-    case url of
-        Left x -> f x
-        Right x -> f x
-    where
-        f :: (MonadIO m, Monoid w, IsString e) => (Url scheme, Option scheme) -> BugT e w m ()
-        f (u,o) = do
-            resp <- req GET u NoReqBody bsResponse o
-            liftIO . BSC'.putStrLn $ responseBody resp
--}
-
 -- | The preferred normalization. Convenience function.
 normalize :: URIRef a -> BS'.ByteString
 normalize = normalizeURIRef' aggressiveNormalization
@@ -239,9 +224,71 @@ parseURI s = case U.parseURI laxURIParserOptions s of
     Left o -> Left o
     Right r -> pure $ Right r
 
+-- | Produce an absolute URL from a @parseURI@ value
+relTo :: URIRef a -> URIRef Absolute -> URIRef Absolute
+parsed `relTo` (URI {..}) = case parsed of -- URI and RelativeRef have different field names, so wildcards are safe
+    RelativeRef {..} -> URI uriScheme uriAuthority rrPath rrQuery rrFragment -- make parsed relative to the absolute URIRef
+    a@(URI _ _ _ _ _) -> a -- parsed was absolute already; keep as-is
+
+-- | The most convenient way to parse a URI for a scraper (considering that a scraper is seeded with an initial URL, and stays within that URL's domain.) Combines @parseURI@, @relTo@, and @uriToUrl@.
+--
+-- @Left Nothing@ means non-https scheme; other @Left@'s are @URIParseError@s
+--
+-- Because @parseRelTo@ returns in non-monadic @Either@ (in innermost @Either@,) given that you'll usually consider a @Url 'Http@ equal to a @Url 'Https@, use '\(f :: (Url scheme, Option scheme) -> IO ()) -> f ||| f' over the object returned in *Either*.
+parseRelTo :: URIRef Absolute -> BS'.ByteString -> Either (Maybe URIParseError) (Either (Url 'Http, Option 'Http) (Url 'Https, Option 'Https))
+parseRelTo base bs = case parseURI bs of
+    Left l -> Left (Just l)
+    Right (either (`relTo` base) id -> absOrRel) -> liftME Nothing $ uriToUrl absOrRel
+
+-- | 'reqBr' with 'getHttpResponse'. Left here in case it's useful, perhaps for JSON consumption, if there're no JSON conduit libraries. Usually use 'reqBc', through; any I/O should be done via Conduit.
+req :: (MonadHttp m, MonadThrow m, HttpMethod method, HttpBody body, HttpResponse response, HttpBodyAllowed (AllowsBody method) (ProvidesBody body))
+    => method
+    -> URIRef Absolute
+    -> body
+    -> Proxy response
+    -> (forall scheme. Option scheme)
+    -> m response
+req m uri body p opts = reqHelper opts uri $ \(u,o) -> Req.req m u body p o
+
+-- | Uses 'responseBodySource' to consume HTTP response body. 'BsResponse', 'IgnoreResponse', and 'JsonResponse' all work with this, as each instances 'HttpResponse'.
+reqBc :: (MonadHttp m, MonadThrow m, HttpMethod method, HttpBody body, HttpBodyAllowed (AllowsBody method) (ProvidesBody body))
+      => method
+      -> URIRef Absolute
+      -> body
+      -> (forall scheme. Option scheme)
+      -> (forall m. MonadResource m => ConduitT BS'.ByteString Void m a) -- ^ e.g. 'sinkFile'
+      -> m a
+reqBc m uri body opts consumer = reqHelper opts uri $ \(u,o) ->
+    Req.reqBr m u body (o <> opts) (\respBodyReader -> runConduitRes $ responseBodySource respBodyReader .| consumer)
+
+-- | Low-level. Use at your own risk. Left here because it's easy to write, so we may as well make it available here (not re-exported because takes a @URIRef Absolute@ rather than a @Url s@)
+req' :: (MonadHttp m, MonadThrow m, HttpMethod method, HttpBody body, HttpBodyAllowed (AllowsBody method) (ProvidesBody body))
+     => method
+     -> URIRef Absolute
+     -> body
+     -> (forall scheme. Option scheme)
+     -> (Request -> Manager -> m a)
+     -> m a
+req' m uri body opts f = reqHelper opts uri $ \(u,o) -> Req.req' m u body (o <> opts) f
+
+-- ** Helper/static functions (do not export)
+
+type UO s = (Url s, Option s)
+type EUO = Either (UO 'Http) (UO 'Https)
+
+reqHelper :: MonadThrow m
+          => (forall scheme. Option scheme)
+          -> URIRef Absolute
+          -> (forall scheme. UO scheme -> m a)
+          -> m a
+reqHelper o u f = maybe (throw NonHTTP) (f ||| f) -- return function or throw NonHTTP. Cane use (|||) because f is [must be] forall schemes.
+                . fmap (bimap (second (<>o)) (second (<>o))) -- apply generic options; can't use 'both' because o is reified to either
+                                                             -- Option 'Http or Option 'Https, which are two different types!
+                $ uriToUrl u -- convert to EUO
+
 -- TODO: parse basic authentication (user:pass@domain) and include as (Option 'Https) via basicAuth
 -- | Returns @Nothing@ on non-http(s) schemes.
-uriToUrl :: URIRef Absolute -> Maybe (Either (Url 'Http, Option 'Http) (Url 'Https, Option 'Https))
+uriToUrl :: URIRef Absolute -> Maybe EUO
 uriToUrl (URI {..}) =
     let p :: Url a -> Url a -- type signatures given to keep polymorphism
         p z = foldl' (\b a -> b /: decodeUtf8 a) z (BSC'.split '/' uriPath)
@@ -257,52 +304,3 @@ uriToUrl (URI {..}) =
         "http"  -> g Left http
         "https" -> g Right https
         _ -> empty
-
--- | Produce an absolute URL from a @parseURI@ value
-relTo :: URIRef a -> URIRef Absolute -> URIRef Absolute
-parsed `relTo` (URI {..}) = case parsed of -- URI and RelativeRef have different field names, so wildcards are safe
-    RelativeRef {..} -> URI uriScheme uriAuthority rrPath rrQuery rrFragment -- make parsed relative to the absolute URIRef
-    a@(URI _ _ _ _ _) -> a -- parsed was absolute already; keep as-is
-
--- | The most convenient way to parse a URI for a scraper (considering that a scraper is seeded with an initial URL, and stays within that URL's domain.) Combines @parseURI@, @relTo@, and @uriToUrl@.
---
--- @Left Nothing@ means non-https scheme; other @Left@'s are @URIParseError@s
-parseRelTo :: URIRef Absolute -> BS'.ByteString -> Either (Maybe URIParseError) (Either (Url 'Http, Option 'Http) (Url 'Https, Option 'Https))
-parseRelTo base bs = case parseURI bs of
-    Left l -> Left (Just l)
-    Right (either (`relTo` base) id -> absOrRel) -> liftME Nothing $ uriToUrl absOrRel
-
--- | Make a request from an absolute URI. Given how req handles @Url@, there're no compile-time-safe ways to pass-in @Option@s; use _ if you want to pass @Option@s that apply to both @'Http@ and @'Https@ schemes.
-req :: (MonadHttp m, MonadThrow m, HttpMethod method, HttpBody body, HttpResponse response, HttpBodyAllowed (AllowsBody method) (ProvidesBody body))
-    => method
-    -> URIRef Absolute
-    -> body
-    -> Proxy response
-    -> (forall scheme. Option scheme)
-    -> m response
-req m uri body proxy opts = reqHelper (\(u,o) -> Req.req m u body proxy (o <> opts)) uri
-
-reqBr :: (MonadHttp m, MonadThrow m, HttpMethod method, HttpBody body, HttpBodyAllowed (AllowsBody method) (ProvidesBody body))
-      => method
-      -> URIRef Absolute
-      -> body
-      -> (forall scheme. Option scheme)
-      -> (Response BodyReader -> IO a)
-      -> m a
-reqBr m uri body opts f = reqHelper (\(u,o) -> Req.reqBr m u body (o <> opts) f) uri
-
-req' :: (MonadHttp m, MonadThrow m, HttpMethod method, HttpBody body, HttpBodyAllowed (AllowsBody method) (ProvidesBody body))
-     => method
-     -> URIRef Absolute
-     -> body
-     -> (forall scheme. Option scheme)
-     -> (Request -> Manager -> m a)
-     -> m a
-req' m uri body opts f = reqHelper (\(u,o) -> Req.req' m u body (o <> opts) f) uri
-
--- | CPS helper (do not export)
-reqHelper :: (MonadHttp m, MonadThrow m)
-          => (forall scheme. (Url scheme, Option scheme) -> m response)
-          -> URIRef Absolute
-          -> m response
-reqHelper f = maybe (throw NonHTTP) (either f f) . uriToUrl
